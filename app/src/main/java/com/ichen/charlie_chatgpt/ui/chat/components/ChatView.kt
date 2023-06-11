33f0d7f974602bc1092773f8315aa9c729f39e3f
Ian Chen <ichen2@lsu.edu>
2023-06-11 15:24:34 -0400

    Added splash screen, updated icon, and added copy-to-clipboard

--- app/src/main/java/com/ichen/charlie_chatgpt/ui/chat/components/ChatView.kt
+++ app/src/main/java/com/ichen/charlie_chatgpt/ui/chat/components/ChatView.kt
@@ -1,28 +1,45 @@
 package com.ichen.charlie_chatgpt.ui.chat.components
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.ichen.charlie_chatgpt.ui.theme.CharlieTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ChatView(
     message: String?,
     isUser: Boolean,
 ) {
+    val context = LocalContext.current
+    val clipboardManager by remember(context) {
+        derivedStateOf { context.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager }
+    }
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = if(isUser) Arrangement.End else Arrangement.Start,
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = if (isUser) Arrangement.End else Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Box(
@@ -34,6 +51,23 @@ fun ChatView(
                         bottomStart = if (isUser) 24.dp else 0.dp,
                         bottomEnd = if (isUser) 0.dp else 24.dp,
                     )
+                )
+                .combinedClickable(
+                    interactionSource = remember {
+                        MutableInteractionSource()
+                    },
+                    indication = rememberRipple(bounded = true),
+                    onClick = {},
+                    onLongClickLabel = "copy message",
+                    onLongClick = {
+                        clipboardManager?.let { clipboard ->
+                            val textToCopy = ClipData.newPlainText("chat message", message)
+                            clipboard.setPrimaryClip(textToCopy)
+                            Toast
+                                .makeText(context, "Message copied to clipboard", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    },
                 )
                 .background(if (isUser) CharlieTheme.colors.primary else CharlieTheme.colors.surface)
                 .padding(horizontal = 24.dp, vertical = 16.dp),
@@ -67,7 +101,7 @@ fun Loading() {
     Row(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        for (i in MIN_NUM_DOTS .. MAX_NUM_DOTS) {
+        for (i in MIN_NUM_DOTS..MAX_NUM_DOTS) {
             Text(
                 text = ".",
                 color = if (numDots > i) CharlieTheme.colors.onSurface else Color.Transparent,
